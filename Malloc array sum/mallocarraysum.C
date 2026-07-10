2c0c297181d5dc2a80dff3c4abfee5238bfc4ef6
evhenhodes-a11y <evhen.hodes@gmail.com>
2026-07-10 23:54:14 +0300

    Fixed file name

---
+++ Malloc array sum/mallocarraysum.C
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int n;
+    printf("Enter how many integers will be in the array:\n");
+    scanf("%d", &n);
+    int *arr = (int *)malloc(n * sizeof(int));
+    if (arr == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    printf("Enter %d integers:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    printf("Sum of array elements: %d\n", sum);
+
+    free(arr);
+
+    return 0;
+}
